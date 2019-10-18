05773a3f932ba591d04d6e2915d84c9c1527abaa
Souhail <mr.souid@live.fr>
2019-10-18 09:04:21 +0200

    Page Accueil connected

--- css/styles.js
+++ css/styles.js
@@ -5,23 +5,17 @@ export default StyleSheet.create({
   'import': '"../ma_formation/livretAccueil.css"',
   'import': '"../ma_formation/agenda.css"',
   'import': '"../admin/admin.css"',
-  'nav': {
-    'display': 'flex',
-    'justifyContent': 'flex-start'
+  'import': '"../ma_formation/AccueilConnected.css"',
+  'nav > div': {
+    'paddingLeft': [{ 'unit': 'rem', 'value': 1 }],
+    'paddingRight': [{ 'unit': 'rem', 'value': 1 }]
   },
-  'nav ul': {
-    'listStyle': 'none'
-  },
-  'nav > div:last-child': {
+  'nav > button:last-child': {
     'marginLeft': [{ 'unit': 'string', 'value': 'auto' }],
-    'marginRight': [{ 'unit': 'px', 'value': 80 }]
+    'marginRight': [{ 'unit': 'px', 'value': 50 }]
   },
   'nav-title': {
     'fontWeight': 'bold'
-  },
-  '#iframe-navbar': {
-    'width': [{ 'unit': 'vw', 'value': 100 }],
-    'height': [{ 'unit': 'rem', 'value': 11 }]
   },
   'main': {
     'margin': [{ 'unit': 'px', 'value': 150 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }]
---
+++ ma_formation/AccueilConnected.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'accueil-connected': {
+    'margin': [{ 'unit': 'rem', 'value': 0.7 }, { 'unit': 'rem', 'value': 0.7 }, { 'unit': 'rem', 'value': 0.7 }, { 'unit': 'rem', 'value': 0.7 }]
+  },
+  'carousel-connected': {
+    'height': [{ 'unit': 'vh', 'value': 90 }]
+  }
+});
