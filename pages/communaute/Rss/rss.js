836770bd3720a51a440dd0e6d8303ad17d04bb07
Souhail <mr.souid@live.fr>
2019-10-29 14:02:52 +0100

    fix footer height, upgrade page rss and iframe livret accueil

--- pages/communaute/Rss/rss.js
+++ pages/communaute/Rss/rss.js
@@ -4,12 +4,21 @@ import Card from '../../../components/Card/card'
 import '../../../styles/sass/styles.scss'
 const elementCardRss = (
 	<article className="card-rss-article">
-		<h1 className="card-rss-title">Apprenez en plus sur Javascript !</h1>
-		<p className="card-rss-description">
-			Dans cet article nous traiterons des spécificités de Javascript ainsi que des concepts clés à maîtriser
-		</p>
-		<a href="https://fr.khanacademy.org/computing/computer-programming" target="_blank">
-			<button className="card-rss-button">Lien vers article</button>
+		<a
+			title="lien vers le flux"
+			href="https://fr.khanacademy.org/computing/computer-programming"
+			target="_blank"
+			style={{ color: '#fff' }}
+		>
+			<section style={{ display: 'flex', flexWrap: 'wrap' }}>
+				<button className="card-rss-button">
+					<i class="fas fa-rss" />
+				</button>
+				&nbsp;
+				<h1 title="lien vers le flux" className="card-rss-title">
+					Apprenez en plus sur Javascript !
+				</h1>
+			</section>
 		</a>
 	</article>
 )
@@ -18,10 +27,7 @@ const Rss = () => (
 		<section className="section-rss">
 			<h1 className="text-center pageRss-title"> Flux RSS Paris Promo 2</h1>
 			<section className="row">
-				<article className=" col-md-12 col-sm-12 col-xs-12 rss-article">
-					<section className="col-md-5 col-sm-12 col-xs-12 section-rss">
-						<Card styleName="card-rss">{elementCardRss}</Card>
-					</section>
+				<article className=" col-md-12 col-sm-12 col-xs-12">
 					<section className="col-md-5 col-sm-12 col-xs-12 section-rss">
 						<Card styleName="card-rss">{elementCardRss}</Card>
 					</section>
--- pages/ma_formation/LivretAccueil/livret_accueil.js
+++ pages/ma_formation/LivretAccueil/livret_accueil.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import Page from '../../../layouts/classic'
+import '../../../styles/sass/styles.scss'
 
-export default function Livret () {
-  return (
-    <Page title="Contact utiles">
-    </Page>
-  )
+const Livret = () => {
+	return (
+		<Page title="livret">
+			<section className="section-livret">
+				<h1 className="text-center"> Livret Accueil</h1>
+				<article className=" col-md-6 col-sm-12 col-xs-12 section-pdf">
+					<iframe src={`/academy.pdf`} style={{ width: '718px', height: '700px' }} frameborder="0" />
+				</article>
+			</section>
+		</Page>
+	)
 }
+
+export default Livret
