7c850364deae82780e71cf97b80614f92c941a36
Souhail <mr.souid@live.fr>
2019-10-30 13:42:30 +0100

    add components modal, and implement inside pages gestionUtilisateur, then modify button component css and fix body css backgroundcolor, finaly change tex-align to left for who to follow in description 'p'

--- components/Boutons/Boutons.js
+++ components/Boutons/Boutons.js
@@ -1,12 +1,15 @@
 import React from 'react'
+
 import './Boutons.scss'
 
 // btnType = ajout des classes necessaires/classes a mettre dans bouton.scss
 // cliked = envera enventuellement les props du bouton au component parent
 
 const Button = (props) => (
-  <React.Fragment>
-    <button type="button" className={`btn ${props.btnType}`} onClick={props.clicked}>{props.children}</button>
-  </React.Fragment>
+	<React.Fragment>
+		<button type="button" className={`btn ${props.btnType}`} onClick={props.clicked}>
+			{props.children}
+		</button>
+	</React.Fragment>
 )
 export default Button
--- pages/admin/GestionUtilisateur/gestion_utilisateur.js
+++ pages/admin/GestionUtilisateur/gestion_utilisateur.js
@@ -1,50 +1,81 @@
-import React, { Component } from "react"
+import React, { Component } from 'react'
 import Page from '../../../layouts/admin'
 import Button from '../../../components/Boutons/Boutons'
 import Link from 'next/link'
-
+import Alert from '../../../components/Modal/alert'
 class UtilisateursGestion extends Component {
-  state = {
-    utilisateurs : [
-      {name:"Nattan",lastName:"Kifoyi",promotion:"Promo Rio 1", role:"élève", userId:1},
-      {name:"Nattan",lastName:"Kifoyi",promotion:"Promo Rio 1", role:"élève", userId:2},
-      {name:"Nattan",lastName:"Kifoyi",promotion:"Promo Rio 1", role:"élève", userId:3}
-    ]
-  }
+	state = {
+		utilisateurs: [
+			{ name: 'Nattan', lastName: 'Kifoyi', promotion: 'Promo Rio 1', role: 'élève', userId: 1 },
+			{ name: 'Nattan', lastName: 'Kifoyi', promotion: 'Promo Rio 1', role: 'élève', userId: 2 },
+			{ name: 'Nattan', lastName: 'Kifoyi', promotion: 'Promo Rio 1', role: 'élève', userId: 3 }
+		],
+		modalShow: false,
+		show: false
+	}
+	handleModal = (event) => {
+		this.setState({ modalShow: true })
+		event.preventDefault()
+	}
+	handleDelete = (userId) => {
+		const users = this.state.utilisateurs.filter((item) => item.userId !== userId)
+		this.setState({ utilisateurs: users })
+		this.setState({ modalShow: false })
 
-  handleDelete = (userId) =>{
-    alert('Utilisateur supprimé !');
-    const users = this.state.utilisateurs.filter(item => item.userId !== userId);
-    this.setState({utilisateurs : users});
+		event.preventDefault()
+		// alert('Utilisateur supprimé !');
+	}
+	handleClose = (event) => {
+		this.setState({ modalShow: false })
 
-  }
-  render () {
-    return (
-      <Page title="gestion Utilisateur">
-      <article className="gestionProgramme card">
-        <header className="card-header">
-        Liste des Utilisateurs
-        </header>
-        <section className="card-body">
-          <ul>
-            {this.state.utilisateurs.map((utilisateur, index) =>(
-              <li key={index} className="d-flex flex- justify-content-around align-items-baseline">
-                <a href="#">{utilisateur.name} {utilisateur.lastName}</a>
-                <Button btnType="annuler" clicked={(userId) => this.handleDelete(utilisateur.userId)}>Supprimer</Button>
-                <Button btnType="modifier"><Link href="./modificationUtilisateur"><a>Modifier</a></Link></Button>
-              </li>
-            ))}
-          </ul>
-        </section>
-        <footer className="d-flex flex-row align-items-end justify-content-center">
-          <Link href="/admin/CreationUtilisateur/creation_utilisateur">
-            <button type="button" className="btn btn-primary">
-            Ajouter un utilisateur
-            </button></Link>
-        </footer>
-      </article>
-      </Page>
-    )
-  }
+		event.preventDefault()
+	}
+	render() {
+		return (
+			<Page title="gestion Utilisateur">
+				<article className="gestionProgramme card">
+					<header className="card-header">Liste des Utilisateurs</header>
+					<section className="card-body">
+						<ul>
+							{this.state.utilisateurs.map((utilisateur, index) => (
+								<li key={index} className="d-flex flex- justify-content-around align-items-baseline">
+									<a href="#">
+										{utilisateur.name} {utilisateur.lastName}
+									</a>
+									<Button btnType="annuler" clicked={this.handleModal}>
+										Supprimer
+									</Button>
+									{this.state.modalShow ? (
+										<Alert
+											show={this.state.modalShow}
+											handleClose={this.handleClose}
+											handleDelete={() => this.handleDelete(utilisateur.userId)}
+											headerTitle="Suppression utilisateur"
+											modalDescription="Etes vous sûr de vouloir supprimer cet utilisateur ?"
+											modalHeader={true}
+											modalBody={true}
+											modalFooter={true}
+										/>
+									) : null}
+									<Link href="./modificationUtilisateur">
+										<a>
+											<Button btnType="valider">Modifier</Button>
+										</a>
+									</Link>
+								</li>
+							))}
+						</ul>
+					</section>
+					<footer className="d-flex flex-row align-items-end justify-content-center">
+						<Link href="/admin/CreationUtilisateur/creation_utilisateur">
+							<button type="button" className="btn btn-primary">
+								Ajouter un utilisateur
+							</button>
+						</Link>
+					</footer>
+				</article>
+			</Page>
+		)
+	}
 }
 export default UtilisateursGestion
