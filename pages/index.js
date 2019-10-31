af5521696b2d8d73a43a37c4e8e721e52651f930
Giret Firmin <zephimir@gmail.com>
2019-10-31 15:38:31 +0100

    the fuck is this

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,24 @@ import React from 'react'
 import Page from '../layouts/accueil'
 import CarouselZenika from '../components/Index/Carousel/carousel'
 import Login from '../components/Login/login'
+<<<<<<< HEAD
 
 const Home = () => (
   <Page title="Accueil">
     <CarouselZenika />
     <Login />
   </Page>
+=======
+import {Button, ButtonToolbar} from 'react-bootstrap'
+
+const Home = () => (
+  <React.Fragment>
+    <Page title="Accueil">
+      <Carousel />
+      <Login />
+    </Page>
+  </React.Fragment>
+>>>>>>> parent of 340de33... navbar
 )
 
 export default Home
