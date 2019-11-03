cd932457257bc242648d85ab0927c1ee83f65a9c
unknown <Kifoyinattan@gmail.com>
2019-11-03 18:38:24 +0100

    Changements du component boutons ajout des champs required

--- pages/communaute/Ressources/ajouterRessource.js
+++ pages/communaute/Ressources/ajouterRessource.js
@@ -4,7 +4,7 @@ import Button from '../../../components/Boutons/Boutons'
 
 class AjouterRessources extends Component {
   handleValidation () {
-    alert('Votre Ressource a été ajouté')
+
   }
 
   render () {
@@ -14,19 +14,25 @@ class AjouterRessources extends Component {
           <h1>Ajouter une ressource</h1>
           <section>
             <form className="form-group d-flex flex-column">
-              <label>Titre</label>
-              <input className="form-control" type="text"/>
-              <label>Ressource</label>
-              <textarea className="form-control">
-              </textarea>
-              <label>Mots Clé</label>
-              <select className="form-control" type="text">
+              <label for="titre">Titre</label>
+              <div className="d-flex flex-row">
+                <input id="titre" className="form-control" type="text" required/>
+                <span>&nbsp;*</span>
+              </div>
+              <label for="ressource">Ressource</label>
+              <div className="d-flex flex-row">
+                <textarea for="ressource" className="form-control" required>
+                </textarea>
+                <span>&nbsp;*</span>
+              </div>
+              <label for="modules" >Modules associés</label>
+              <select id="modules" className="form-control" type="text" required>
                 <option value="HTML">HTML</option>
                 <option value="CSS">Css</option>
                 <option value="Javascript">Javascript</option>
               </select>
+              <Button btnType="valider" submit={true} clicked={this.handleValidation}>Ajouter</Button>
             </form>
-            <Button btnType="valider" clicked={this.handleValidation}>Ajouter</Button>
           </section>
         </article>
       </Page>
--- pages/communaute/Ressources/modifierRessource.js
+++ pages/communaute/Ressources/modifierRessource.js
@@ -11,11 +11,11 @@ class AjouterRessources extends Component {
   }
 
   handleValidation = () => {
-    this.setState({modalShow: true})
-    setTimeout(() => {
-			this.setState({ modalShow: false })
-			window.location.assign('./ressources')
-		}, 5000)
+    // this.setState({modalShow: true})
+    // setTimeout(() => {
+		// 	this.setState({ modalShow: false })
+		// 	window.location.assign('./ressources')
+		// }, 5000)
   }
 
   render () {
@@ -24,24 +24,30 @@ class AjouterRessources extends Component {
         <article className="ajouterRessource">
           <h1>Modifier une ressource</h1>
           <section>
-            <form className="form-group d-flex flex-column" aria-labelledby="ressource">
-              <label for="titreRessource">Titre de la ressource</label>
-              <input id="titreRessource" className="form-control" type="text" value="" placeholder="Apprendre le html en 5 min" aria-required="true"/>
+            <form className="form-group d-flex flex-column">
+              <label for="titre">Titre de la ressource</label>
+              <div class="d-flex flex-row">
+                <input id="titre" className="form-control" type="text" placeholder="Apprendre le html en 5 min" aria-required="true" required/>
+                <span>&nbsp;*</span>
+              </div>
               <label for="ressource">Ressource</label>
-              <textarea id="ressource" className="form-control" aria-required="true">
-                Laborum magna eu exercitation aliquip adipisicing velit dolor pariatur duis esse. Do est aliquip id velit. Elit pariatur irure et sint quis qui. Lorem eiusmod voluptate exercitation sunt elit non dolore dolore pariatur do incididunt. Velit ullamco sint sit qui cupidatat aute fugiat fugiat ex sint.
-              </textarea>
+              <div className="d-flex flex-row">
+                <textarea id="ressource" className="form-control" aria-required="true" required>
+                  Laborum magna eu exercitation aliquip adipisicing velit dolor pariatur duis esse. Do est aliquip id velit. Elit pariatur irure et sint quis qui. Lorem eiusmod voluptate exercitation sunt elit non dolore dolore pariatur do incididunt. Velit ullamco sint sit qui cupidatat aute fugiat fugiat ex sint.
+                </textarea>
+                <span>&nbsp;*</span>
+              </div>
               <label for="modules">Modules associé</label>
-              <select id="modules" className="form-control" type="text" aria-required="true">
+              <select id="modules" className="form-control" type="text" aria-required="true" required>
                 <option value="HTML">HTML</option>
                 <option value="CSS">Css</option>
                 <option value="Javascript">Javascript</option>
               </select>
-            </form>
-            <div className="d-flex flex-row justify-content-end">
+              <div className="d-flex flex-row justify-content-end">
               <Button btnType="annuler"><Link href="./ressources"> Annuler</Link></Button>
-              <Button btnType="valider" clicked={this.handleValidation}>Valider</Button>
+              <Button btnType="valider" submit={true} clicked={this.handleValidation}>Valider</Button>
             </div>
+            </form>
           </section>
         </article>
         {this.state.modalShow ? (
