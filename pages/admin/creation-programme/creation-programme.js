68e59c23fc6ab16ce1bd9ccefd935b49262c340c
Souhail <mr.souid@live.fr>
2019-11-06 21:46:42 +0100

    required in input

--- pages/admin/creation-programme/creation-programme.js
+++ pages/admin/creation-programme/creation-programme.js
@@ -4,12 +4,16 @@ import Button from '../../../components/Boutons/Boutons'
 import Alert from '../../../components/Modal/alert'
 class CreaProgramme extends Component {
   state = {
-
+    name: '',
     show: false
   }
 
   handleUpdate = () => {
-    this.setState({ show: true })
+    // if (name === '' || undefined) {
+    //   this.setState({ show: false })
+    // } else {
+    return this.setState({ show: false })
+    // }
   }
 
   handleConfirmForm = () => {
@@ -30,16 +34,16 @@ class CreaProgramme extends Component {
           <header className="card-header text-center">
             Creation programme
       </header>
-          <form className="container" >
+          <form className="container"  >
             <section className="section">
               <div className="form-group">
                 <label htmlFor="programtitle">Titre</label><br></br>
-                <input type="text" name="programtitle" required={true} className="form-control" id="exampleFormControlInput1"
+                <input type="text" name="programtitle" required className="form-control" id="exampleFormControlInput1" value={this.state.name} onChange={e => this.onTodoChange(e.target.value)}
                   placeholder="Intitulé du programme" />
               </div>
               <div data-test-hook="groups">
                 <label htmlFor="modules">Choisir module(s)</label>
-                <select className="form-control" name="modules" id="choices-groups" multiple>
+                <select className="form-control" name="modules" id="choices-groups" multiple required >
                   <optgroup label="DEVOPS">
                     <option value="Paris">Linux</option>
                     <option value="Lyon">Mac</option>
@@ -66,7 +70,7 @@ class CreaProgramme extends Component {
                 <div className="input-group-prepend">
                   <span className="input-group-text description-programme-span">Description</span>
                 </div>
-                <textarea className="form-control" name="description" aria-label="With textarea"></textarea>
+                <textarea className="form-control" name="description" aria-label="With textarea" required ></textarea>
               </div>
             </section>
             <section className="d-flex flex-row footer-programme-formulaire">
