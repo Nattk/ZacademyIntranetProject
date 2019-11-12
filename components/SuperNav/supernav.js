6d4937835517325e2b57763800a95e47b17ce0c2
Giret Firmin <zephimir@gmail.com>
2019-11-12 17:00:27 +0100

    firs test db

--- components/SuperNav/supernav.js
+++ components/SuperNav/supernav.js
@@ -1,79 +1,51 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Nav from '../Nav/nav'
 import AdminNav from '../AdminNav/adminNav'
-import Login from '../../components/Login/login'
+import loginService from '../../services/login'
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/users'
 
 const SuperNav = () => {
-  const [isAdmin, SetAdmin] = useLocalStorage('isAdmin', false)
-  const [isConnected, SetConnected] = useLocalStorage('isConnected', false)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [role, setRole] = useState(null)
 
-  const handleAdminON = () => {
-    SetAdmin(true)
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    const user = await loginService.login({
+      username, password, role
+    })
+    setRole(user.role)
+    setUsername('')
+    setPassword('')
   }
 
-  const handleAdminOFF = () => {
-    SetAdmin(false)
+  if (true) {
+    return (
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    )
   }
-
-  const handleOffline = () => {
-    SetConnected(false)
-    SetAdmin(false)
-  }
-
-  const handleOnline = () => {
-    SetConnected(true)
-    SetAdmin(false)
-  }
-
-  const handleboth = () => {
-    SetConnected(true)
-    SetAdmin(true)
-  }
-
-  if (!isConnected) {
-    return <Login online={handleOnline} both={handleboth}/>
-  }
-  if (isAdmin) {
-    return <AdminNav adminClick={handleAdminOFF} offline={handleOffline} />
-  } else {
-    return <Nav adminClick={handleAdminON} offline={handleOffline}/>
-  }
-}
-
-function useLocalStorage (key, initialValue) {
-  // State to store our value
-  // Pass initial state function to useState so logic is only executed once
-  const [storedValue, setStoredValue] = useState(() => {
-    try {
-      // Get from local storage by key
-      const item = window.localStorage.getItem(key)
-      // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue
-    } catch (error) {
-      // If error also return initialValue
-      console.log(error)
-      return initialValue
-    }
-  })
-
-  // Return a wrapped version of useState's setter function that ...
-  // ... persists the new value to localStorage.
-  const setValue = value => {
-    try {
-      // Allow value to be a function so we have same API as useState
-      const valueToStore =
-          value instanceof Function ? value(storedValue) : value
-      // Save state
-      setStoredValue(valueToStore)
-      // Save to local storage
-      window.localStorage.setItem(key, JSON.stringify(valueToStore))
-    } catch (error) {
-      // A more advanced implementation would handle the error case
-      console.log(error)
-    }
-  }
-
-  return [storedValue, setValue]
 }
 
 export default SuperNav
