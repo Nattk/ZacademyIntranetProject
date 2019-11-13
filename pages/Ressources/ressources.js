e42212d87a3d89a75bed0df316048ea76bc2a05c
Nattan <kifoyinattan@gmail.com>
2019-11-13 15:44:12 +0100

    select states

--- pages/Ressources/ressources.js
+++ pages/Ressources/ressources.js
@@ -3,25 +3,51 @@ import Page from '../../layouts/classic'
 import Card from '../../components/Card/card'
 import Button from '../../components/Boutons/Boutons'
 import Link from 'next/link'
+import Select from 'react-select';
 
 class Ressources extends Component{
 
   state = {
     ressources: [
-      {titre:"Apprendre le HTML en 5min" , lien:"ressource-individuelle" , promotion:"Promo Rennes 2", auteur:"Nattan Kifoyi", promoId:1 , contributeur:1, ressId:1},
-      {titre:"Apprendre le HTML en 5min" , lien:"ressource-individuelle" , promotion:"Promo Rennes 2", auteur:"Nattan Kifoyi", promoId:1 , contributeur:1, ressId:2},
-      {titre:"Apprendre le HTML en 5min" , lien:"ressource-individuelle",  promotion:"Promo Paris 2", auteur:"Nattan Kifoyi", promoId:2, contributeur:2, ressId:3},
-      {titre:"Apprendre le HTML en 5min" , lien:"ressource-individuelle",  promotion:"Promo Rio 1", auteur:"Nattan Kifoyi", promoId:3, contributeur:2, ressId:4}
+      {titre:"Apprendre le JAVA", lien:"ressource-individuelle" , promotion:"Promo Rennes 2", auteur:"Nattan Kifoyi", promoId:1 , contributeur:1, ressId:1},
+      {titre:"Les algorithmes JAVA", lien:"ressource-individuelle" , promotion:"Promo Rennes 2", auteur:"Nattan Kifoyi", promoId:1 , contributeur:1, ressId:2},
+      {titre:"Revue de code en JAVA", lien:"ressource-individuelle",  promotion:"Promo Paris 2", auteur:"Nattan Kifoyi", promoId:2, contributeur:2, ressId:3},
+      {titre:"TDD", lien:"ressource-individuelle",  promotion:"Promo Rio 1", auteur:"Nattan Kifoyi", promoId:2, contributeur:2, ressId:4},
+      {titre:"Les collections", lien:"ressource-individuelle",  promotion:"Promo Rio 1", auteur:"Nattan Kifoyi", promoId:3, contributeur:2, ressId:5},
+      {titre:"Les dates", lien:"ressource-individuelle",  promotion:"Promo Rio 1", auteur:"Nattan Kifoyi", promoId:3, contributeur:2, ressId:6}
     ],
-    filtrePromo: 0,
+    Promotions: [
+      {ville:"Paris", id:1, promotion:"Paris 01", programme:'Consultant Javascript', date:'12/12/19'},
+      {ville:"Casablanca", id:3, promotion:"Casablanca 01", programme:'Consultant Java', date:'12/12/19'},
+      {ville:"Rennes", id:5, promotion:"Rennes 02", programme:'Consultant Java', date:'12/12/19'},
+    ],
+
+    Modules: [
+      {id:0, name:'Algorithme java'},
+      {id:1, name:'Librairies JDK'},
+      {id:3, name:'Craftmanship'}
+    ],
+
+    Sequences: [
+      {id:1, idModules:1, name:'dates'},
+      {id:2, idModules:1, name:'collections'},
+      {id:3, idModules:3, name:'Pair Programming'},
+      {id:4, idModules:3, name:'Revue de code'},
+      {id:5, idModules:3, name:'TDD'},
+      {id:6, idModules:3, name:'TDD'}
+    ],
+    filtrePromo: [],
+    filtreModules: [],
+    filtreSequence: [],
     ressourcesDispo:0,
-    formateurs: false,
+    formateurs: false
     }
 
 
   filtre = (event) => {
-
-      this.setState({filtrePromo: parseInt(event.target.value)})
+    // event.map(content => (
+    //   this.setState({filtrePromo : })
+    // ))
   }
 
   handleDelete = (ressId) =>{
@@ -69,35 +95,38 @@ class Ressources extends Component{
     <Page title="Ressources" contextePage="Ressources">
       <article className="ressources d-flex flex-column">
         <header className="d-flex flex-row justify-content-between align-items-center">
-            <select defaultValue="0" className="custom-select" id="selectPromo" onChange={this.filtre}>
-              <option value="0">Promotions</option>
-              <option value="1">Promo Rennes 2</option>
-              <option value="2">Promo Paris 2</option>
-              <option value="3">Promo Rio 1</option>
-            </select>
-            <select defaultValue="0" className="custom-select" id="selectKeywords" >
-              <option value="0">Modules</option>
-              <option value="1">Javscript</option>
-              <option value="2">Craft</option>
-            </select>
-            <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown button
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-              <label class="form-check-label" for="exampleCheck1">Check me out</label>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-            </div>
-            <Button btnType="valider">
-              <Link href="./ajouter-ressource">
-                <a>
-                  Ajouter une ressource
-                </a>
-              </Link>
-            </Button>
+          <Select className="select-component" options={this.state.Promotions} isMulti={true}
+            formatCreateLabel={(inputValue) => `Promotion`}
+            noOptionsMessage={(inputValues) => `${inputValues.inputValue} cette optio...`}
+            defaultValue="Promotion"
+            placeholder="Promotions"
+            getOptionLabel={(option) => option.promotion}
+            getOptionValue={(option) => option.id}
+            onChange={this.filtre}
+          ></Select>
+          <Select className="select-component" options={this.state.Modules} isMulti={true}
+          formatCreateLabel={(inputValue) => `Modules`}
+          noOptionsMessage={(inputValues) => `${inputValues.inputValue} cette optio...`}
+          defaultValue="Promotion"
+          placeholder="Modules"
+          getOptionLabel={(option) => option.name}
+          getOptionValue={(option) => option.id}
+          ></Select>
+          <Select className="select-component" options={this.state.Sequences} isMulti={true}
+          formatCreateLabel={(inputValue) => `Sequences`}
+          noOptionsMessage={(inputValues) => `${inputValues.inputValue} cette optio...`}
+          defaultValue="Promotion"
+          placeholder="Séquences"
+          getOptionLabel={(option) => option.name}
+          getOptionValue={(option) => option.id}
+          ></Select>
+          <Button btnType="valider">
+            <Link href="./ajouter-ressource">
+              <a>
+                Ajouter une ressource
+              </a>
+            </Link>
+          </Button>
         </header>
         <aside className="align-self-stretch">
             <i>{this.state.ressources.length} ressources trouvées</i>
