d1dff74bb9b9ffcaeee92e29911e3c022cbcec0f
Nattan <kifoyinattan@gmail.com>
2019-11-27 20:18:50 +0100

    add packages and babel config for test environnement

---
+++ components/Boutons/Boutons.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react'
+import Button from './Boutons'
+
+describe('Creation Programme', () => {
+  test('renders content', () => {
+    const component = render(<Button />)
+  })
+})
