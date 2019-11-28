97cabbc2475b6ceca894f4a93183f6f506eab2f2
Nattan <kifoyinattan@gmail.com>
2019-11-28 16:52:15 +0100

    deleted files

--- components/Card/card.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Card = (props) => (
-  <section className={`card ${props.styleName}`}>
-    {props.children}
-  </section>
-)
-
-export default Card
--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Page from '../layouts/classic'
-
-const Home = () => (
-  <Page title="Accueil">
-  </Page>
-)
-
-export default Home
