2b0e541b8c06681c190d2bf0084c92bd57f6cd4a
Nattan <kifoyinattan@gmail.com>
2019-12-02 16:21:00 +0100

    add component creation-programme

---
+++ client/components/Creation-programme/creation-programme.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import Select from 'react-select'
+import { addToProgram, getModules, create } from '../../services/creation-programme'
+import Button from '../Boutons/Boutons'
+
+class CreationProgramme extends Component {
+    state = {
+      title: '',
+      items: '',
+      selected: ''
+    }
+
+    componentDidMount () {
+      getModules(this.props.name).then(reponse => {
+        console.table(reponse.data)
+        this.setState({ items: reponse.data })
+      }).catch(err => {
+        alert(err, 'getModules')
+      })
+    }
+
+    handleChange = (event) => {
+      console.log(event)
+      this.setState({ title: event.value })
+    }
+
+    handleSelect = (newValue, action) => {
+      console.log(action)
+      switch (action) {
+        case 'select-option':
+          this.setState({ selected: newValue })
+          break
+        case 'remove-value':
+          this.setState({ selected: newValue })
+          break
+        case 'clear':
+          this.setState({ selected: '' })
+          break
+        default:
+          break
+      }
+    }
+
+    handleCreate = () => {
+      event.preventDefault()
+      create(this.props.name, this.state.title).then(response => {
+        console.table(response.data)
+        return getModules(this.props.name)
+      }).then(reponse => {
+        this.setState({ items: reponse.data })
+      })
+        .catch(err => {
+          alert(err, 'handleCreate')
+        })
+    }
+
+    handleAdd = () => {
+      event.preventDefault()
+      console.table(this.state.selected)
+      addToProgram(this.state.selected.id, this.props.name, this.props.programmeId).then(response => {
+        console.table(response.data)
+      }).catch(err => {
+        alert(err, 'handleAdd')
+      })
+    }
+
+    render () {
+      return (
+        <form className="container" >
+          <h2>Ajouter un {this.props.name}</h2>
+          <section className="d-flex flex-row">
+            <input type="text" placeholder="modules" onChange={() => this.handleChange(event.target)}/>
+            <button onClick={this.handleCreate} className="btn btn-primary text-center"
+            >Créer votre {this.props.name}
+            </button>
+          </section>
+          <h2>Selectionner votre {this.props.name}</h2>
+          <Select className="select-component" options={this.state.items}
+            formatCreateLabel={(inputValue) => inputValue}
+            getOptionLabel={(option) => option.title}
+            getOptionValue={(option) => option.title}
+            onChange={(newValue, actionMeta) => this.handleSelect(newValue, actionMeta.action)}
+          />
+          <Button clicked={this.handleAdd}>Ajouter au programme</Button>
+        </form>
+
+      )
+    }
+}
+
+export default CreationProgramme
--- client/services/creation-programme.js
+++ client/services/creation-programme.js
@@ -1,7 +1,17 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3333/api/modules/'
+const baseUrl = 'http://localhost:3333/api/'
 
-export const addModuleToProgram = (modId, progId) => {
-  console.log(`${baseUrl}${modId}`)
-  return axios.put(`${baseUrl}${modId}`, { programmeId: progId })
+export const getModules = (name) => {
+  console.log(`${baseUrl}${name}`)
+  return axios.get(`${baseUrl}${name}`)
+}
+
+export const addToProgram = (nameId, name, progId) => {
+  console.log(`${baseUrl}${name}/${nameId}`)
+  return axios.put(`${baseUrl}${name}/${nameId}`, { programmeId: progId })
+}
+
+export const create = (name, title) => {
+  console.log(name, title)
+  return axios.post(`${baseUrl}${name}`, { title: title })
 }
