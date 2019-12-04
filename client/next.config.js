7c5a406f91b533aae8153a057300319dd82b920b
zephimir <zephimir@gmail.com>
2019-12-04 10:24:14 +0100

    eslint

--- client/next.config.js
+++ client/next.config.js
@@ -1,11 +1,11 @@
 const withCss = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass')
 module.exports = withCss(withSass({
-    webpack: (config) => {
-        return Object.assign({}, config, {
-            node: {
-                fs: 'empty'
-            }
-        })
-    },
+  webpack: (config) => {
+    return Object.assign({}, config, {
+      node: {
+        fs: 'empty'
+      }
+    })
+  }
 }))
