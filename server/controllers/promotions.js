e2df265d27004c504737b6a636e923180afe1949
souhail <mr.souid@live.fr>
2019-12-05 17:21:13 +0100

    get promotion by id && redirect to accueil admin when you created promotion

--- server/controllers/promotions.js
+++ server/controllers/promotions.js
@@ -35,6 +35,8 @@ promotionsRouter.get('/:id', async (req, res, next) => {
           }
         }
       })
+      .populate('formateurs', { firstName: 1, lastName: 1 })
+      .populate('eleves', { firstName: 1, lastName: 1 })
     if (foundpromotion) {
       res.json(foundpromotion.toJSON())
     } else {
