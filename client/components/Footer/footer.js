3d9e40bbfea76b745a43f8e28f4dbccf3939ad47
souhail <mr.souid@live.fr>
2019-12-07 17:29:08 +0100

    change article to section balise in footer when i checked the opquast

--- client/components/Footer/footer.js
+++ client/components/Footer/footer.js
@@ -7,15 +7,15 @@ import '../../styles/sass/styles.scss'
 const Footer = () => (
   <footer className="row footer-decoration ">
     {/* ROW 1 */}
-    <article className="col-md-4 col-sm-6 col-xs-12  row1-position">
+    <section className="col-md-4 col-sm-6 col-xs-12  row1-position">
       <p>
         <span>Contact</span>:&nbsp;
         <a title="Envoyer un mail" href=" mailto:info@zenika.com" className="footer-link-customization"> info@zenika.com
         </a> &nbsp; +33(0)1 45 26 19 15
       </p>
-    </article>
+    </section>
     {/* ROW 2 */}
-    <article className="col-md-4 col-sm-6 col-xs-12 row2-position">
+    <section className="col-md-4 col-sm-6 col-xs-12 row2-position">
       <h1 className="footer-titre-h1 align-center">Zenika Academy</h1>
       <section className="footer-socialIcon">
         {SocialMedia.twitter}
@@ -24,16 +24,16 @@ const Footer = () => (
         {SocialMedia.github}
         {SocialMedia.youtube}
       </section>
-    </article>
+    </section>
     {/* ROW 3 */}
-    <article className="col-md-4 col-sm-6 col-xs-12 row3-position">
+    <section className="col-md-4 col-sm-6 col-xs-12 row3-position">
       <small> &copy; Copyright 2006-2019 Zenika. &shy;Tous droits réservés.
         <section>
           <Link href="/mentions-legales"><a title="Consulter les mentions légales" className="footer-link-customization">Mentions légales
           </a></Link>
         </section>
       </small>
-    </article>
+    </section>
   </footer>
 )
 
