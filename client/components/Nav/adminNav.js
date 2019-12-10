cc4c3bbcdffe517671422bf990ac659891fa9115
zephimir <zephimir@gmail.com>
2019-12-10 09:33:35 +0100

    yo

--- client/components/Nav/adminNav.js
+++ client/components/Nav/adminNav.js
@@ -35,8 +35,8 @@ const AdminNav = () => {
 
           <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
             <Link href="/admin/gestion-programme/gestion-programme"><a role="button" alt="Lien pour gérer les programmes">Liste programme</a></Link><br></br>
-            {/* <Link href="/admin/gestion-promotion/gestion-promotion"><a role="button" alt="Lien pour gérer les promotions">Gestion Promotion</a></Link><br></br>
-            <Link href="/admin/gestion-utilisateur/gestion-utilisateur"><a role="button" alt="Lien pour gérer les utilisateurs">Gestion Utilisateur</a></Link><br></br> */}
+            <Link href="/admin/gestion-promotion/gestion-promotion"><a role="button" alt="Lien pour gérer les promotions">Gestion Promotion</a></Link><br></br>
+            <Link href="/admin/gestion-utilisateur/gestion-utilisateur"><a role="button" alt="Lien pour gérer les utilisateurs">Gestion Utilisateur</a></Link><br></br>
           </div>
         </div>
         <div id="end-of-navbar">
