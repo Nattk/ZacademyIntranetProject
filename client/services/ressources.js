094f42f7b0912361aa44536019258016d41f5e58
zephimir <zephimir@gmail.com>
2019-12-10 16:03:30 +0100

    working

--- client/services/ressources.js
+++ client/services/ressources.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/'
+const baseUrl = 'http://localhost:3333/'
 
 export const getAllRessources = () => {
   return axios.get(baseUrl + 'ressources')
