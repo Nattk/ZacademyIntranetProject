73d2a1de0f2bbf2f6e34f72e1c8b5545a09a28e9
souhail <mr.souid@live.fr>
2019-12-10 15:49:59 +0100

    test

---
+++ client/pages/communaute/who-to-follow/test.js
@@ -0,0 +1,162 @@
+import React from 'react'
+import Page from '../../../layouts/classic'
+import Header from '../../../components/Header/header-button-add'
+import Modal from '../../../components/Modal/modal'
+import ValidationMethod from '../../../components/Methods/ValidationMethod'
+import Formulaire from '../../../components/Formulaire/formulaire'
+import FormulaireWho2Follow from '../../admin/Who-to-Follow/formulaire'
+import { Form, ShowCard, DeleteDescription } from '../../../components/Modal/SectionModal'
+import Data from '../../../components/whoToFollow/data.json'
+import '../../../styles/sass/styles.scss'
+class Follow extends ValidationMethod {
+  constructor (props) {
+    super(props)
+    this.state = {
+      fakeData: Data,
+      id: '',
+      firstName: '',
+      lastName: '',
+      fonction: '',
+      description: '',
+      linkGithub: '',
+      image: '',
+      picture: '',
+      linkLinkedin: '',
+      linkTwitter: '',
+      firstNameValidation: '',
+      lastNameValidation: '',
+      fonctionValidation: '',
+      descriptionValidation: '',
+      imgValidation: '',
+      descriptionDelete: false,
+      formulaire: false,
+      showModal: false,
+      showModalDelete: false,
+      showModalUpdate: false,
+      showAlertSuccess: false,
+      showAlertUpdate: false,
+      formulaireUpdate: false,
+      showAlertDelete: false,
+      formulaireTitleAdd: false,
+      showButton: false
+
+    }
+    this.handleModalAdd = this.handleModalAdd.bind(this)
+    this.handleClose = this.handleClose.bind(this)
+  }
+
+  handleModalAdd () {
+    this.setState({
+      showModal: true, formulaire: true, formulaireTitleAdd: true, formulaireUpdate: false, descriptionDelete: false, firstName: '', lastName: '', fonction: '', description: '', linkLinkedin: '', linkGithub: '', linkTwitter: '', image: ''
+    })
+  }
+
+  handleClose () {
+    this.setState({
+      showModal: false, firstNameValidation: '', lastNameValidation: '', fonctionValidation: '', descriptionValidation: ''
+    })
+  }
+
+  handleSubmit () {
+    const elements = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      fonction: this.state.fonction,
+      image: this.state.image,
+      description: this.state.description.length > 70 ? this.state.description.substring(0, 70) + '...' : this.state.description,
+      linkGithub: this.state.linkGithub,
+      linkReddit: this.state.linkReddit,
+      linkLinkedin: this.state.linkLinkedin,
+      linkTwitter: this.state.linkTwitter,
+      id: Math.random().toString(36).substr(2, 9)
+    }
+    const data = this.state.fakeData
+    const newdata = [...data, elements]
+    console.log(this.state)
+    if (this.state.firstName && this.state.lastName && this.state.fonction && this.state.description !== '') {
+      this.setState({ fakeData: newdata, showAlertSuccess: true, showModal: false, firstNameValidaion: '', lastNameValidation: '', fonctionValidation: '', descriptionValidation: '' })
+      setTimeout(() => {
+        this.setState({
+          showAlertSuccess: false
+        })
+      }, 5000)
+    } else {
+      this.handleValidation()
+    }
+  }
+
+  componentDidMount () {
+    const user = window.localStorage.getItem('user')
+    const role = JSON.parse(user).role
+    console.log(role === 'superadmin')
+    if (role === 'superadmin') {
+      this.setState({ showButton: true })
+      console.log(this.state.showButton)
+    } else {
+      return this.state.showButton
+    }
+    console.log(this.state.showButton)
+  }
+
+  render () {
+    console.log(this.state)
+    const Formul = (
+      <Formulaire handleClose={this.handleClose}
+        buttonName={this.state.formulaireUpdate ? 'Mettre à jour' : 'Ajouter'}
+        clicked={this.state.formulaireUpdate ? () => this.handleUpdate(this.state.id) : this.handleSubmit}
+        onChange={this.onChange} lastName={this.state.lastName}
+        influenceur
+        contact
+        identity
+        image={this.state.image}
+        firstName={this.state.firstName}
+        lastName={this.state.lastName}
+        fonction={this.state.fonction}
+        description={this.state.description}
+        linkGithub={this.state.linkGithub}
+        linkLinkedin={this.state.linkLinkedin}
+        linkTwitter={this.state.linkTwitter}
+        firstNameValidation={this.state.firstNameValidation}
+        lastNameValidation={this.state.lastNameValidation}
+        fonctionValidation={this.state.fonctionValidation}
+        descriptionValidation={this.state.descriptionValidation}
+
+      />
+    )
+    return (
+      <Page title=" Influenceurs" contextePage="Influenceurs" >
+        <article id="who-to-follow" className="col-md-12 col-sm-12 col-xs-12 section-card" >
+          <Header clicked={this.handleModalAdd} showAlertSuccess={this.state.showAlertSuccess} showAlertDelete={this.state.showAlertDelete} showAlertUpdate={this.state.showAlertUpdate} firstName={this.state.firstName} lastName={this.state.lastName} title="Ajouter un influenceur" />
+          <section className="col-md-12 col-sm-12 col-xs-12 section-article" >
+            {this.state.fakeData.map((user, id) => (
+
+              <ShowCard
+                key={id}
+                picture={user.image}
+                firstName={user.firstName}
+                lastName={user.lastName}
+                fonction={user.fonction}
+                description={user.description}
+                linkLinkedin={user.linkLinkedin}
+                linkGithub={user.linkGithub}
+                linkTwitter={user.linkTwitter}
+
+                showButton={!!this.state.showButton}
+                remove={() => this.setState({ showModal: true, descriptionDelete: true, formulaire: false, id: user.id })}
+                update={() => this.setState({
+                  showModal: true, formulaire: true, formulaireUpdate: true, descriptionDelete: false, formulaireTitleAdd: false, id: user.id, firstName: user.firstName, lastName: user.lastName, fonction: user.fonction, description: user.description, linkLinkedin: user.linkLinkedin, linkGithub: user.linkGithub, linkTwitter: user.linkTwitter, image: this.state.img
+                })}
+              />
+            ))}
+            <Modal show={this.state.showModal} onClose={this.handleClose} titleModal={this.state.formulaireTitleAdd ? "Ajout d'un contact" : '' || this.state.formulaireUpdate ? 'Modification du contact' : ''}>
+              {this.state.formulaire ? <FormulaireWho2Follow buttonName={this.state.formulaireUpdate ? 'Mettre à jour' : 'Ajouter'}
+              /> : false}
+              {this.state.descriptionDelete ? <DeleteDescription handleDelete={() => this.handleDelete(this.state.id)} handleClose={this.handleClose} title="Êtes-vous sûr de vouloir supprimer ce profil" /> : false}
+            </Modal>
+          </section>
+        </article>
+      </Page>
+    )
+  }
+}
+export default Follow
