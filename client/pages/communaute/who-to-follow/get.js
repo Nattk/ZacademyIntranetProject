69e5c88ed0f704cd286abec59010971e470b20ce
souhail <mr.souid@live.fr>
2019-12-12 15:29:04 +0100

    delete some files after refactor

--- client/pages/communaute/who-to-follow/get.js
+++
@@ -1,4 +0,0 @@
-import CardContact from '../../../components/CardContact/cardContact'
-export const contacts = (props) => (
-  props.contacts ? props.contacts.map((user, id) => <CardContact key={id} title={user.title} content={user.content} />) : null
-)
