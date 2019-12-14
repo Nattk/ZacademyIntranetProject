fe8f20d5cfd5491eb73b5315b5bbbeadad73ae1d
souhail <mr.souid@live.fr>
2019-12-14 10:06:57 +0100

    prepare contact utiles

---
+++ client/components/Modal/function-modal.js
@@ -0,0 +1,8 @@
+export const handleClose = (updateState) => {
+  updateState({ showModal: false, title: '', content: '', titleValidation: '', contentValidation: '' })
+}
+export const handleModalAdd = (updateState) => {
+  updateState({
+    showModal: true, formulaire: true, formulaireTitleAdd: true, recap: false, formulaireUpdate: false, descriptionDelete: false, showDetails: false, title: '', content: '', titleValidation: '', contentValidation: ''
+  })
+}
