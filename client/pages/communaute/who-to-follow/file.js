6a52972273d1ae7412e8d49b6268f7e643b95a45
souhail <mr.souid@live.fr>
2019-12-14 10:41:48 +0100

    add methods file

--- client/pages/communaute/who-to-follow/file.js
+++ client/pages/communaute/who-to-follow/file.js
@@ -12,7 +12,6 @@ export const configuration = (state) => {
     github: state.github,
     promotionId: JSON.parse(user).promotion
   }
-
   return elements
 }
 
@@ -50,36 +49,4 @@ export const handleRemove = (state, id, updateState) => {
   setTimeout(() => {
     updateState({ showAlertDelete: false })
   }, 3000)
-}
-export const newData = (state, id) => {
-  const data = state.contacts.filter(el => el.id !== id)
-  const newArray = [...data]
-  return newArray
-}
-export const handleClose = (updateState) => {
-  updateState({ showModal: false, title: '', content: '', titleValidation: '', contentValidation: '' })
-}
-export const handleModalAdd = (updateState) => {
-  updateState({
-    showModal: true, formulaire: true, formulaireTitleAdd: true, recap: false, formulaireUpdate: false, descriptionDelete: false, showDetails: false, title: '', content: '', titleValidation: '', contentValidation: ''
-  })
-}
-export const handleValidation = (state, updateState) => {
-  state.title === '' ? updateState({ titleValidation: 'Un titre est requis' }) : updateState({ titleValidation: '' })
-  state.content === '' ? updateState({ contentValidation: 'Une description est requise' }) : updateState({ contentValidation: '' })
-  state.selectedProgramme === '' ? updateState({ programmeValidation: 'Un programme est réquis' }) : updateState({ programmeValidation: '' })
-  state.formateursOption === '' ? updateState({ formateursValidation: 'Veuillez selectionné un ou plusieurs formateurs ' }) : updateState({ formateursValidation: '' })
-  state.studentsOption === '' ? updateState({ studentsValidation: 'Veuillez selectionné des futurs consultants ' }) : updateState({ studentsValidation: '' })
-  state.selectedCity === '' ? updateState({ cityValidation: 'Veuillez selectionné une ville ' }) : updateState({ cityValidation: '' })
-  state.startDate === undefined || state.startDate === null ? updateState({ startdateValidation: 'Veuillez selectionné une date de début de formation ' }) : updateState({ startdateValidation: '' })
-  state.endDate === undefined || state.endDate === null ? updateState({ enddateValidation: 'Veuillez selectionné une date  de fin de formation ' }) : updateState({ enddateValidation: '' })
-  state.startDate === undefined && state.endDate === undefined ? updateState({ dateValidation: 'Veuillez selectionné une période de formation ' }) : updateState({ dateValidation: '' })
-}
-
-export const onShowRecapForm = (state, updateState) => {
-  if (state.title && state.content !== '') {
-    updateState({ recap: true, formulaire: false })
-  } else {
-    handleValidation(state, updateState)
-  }
 }
