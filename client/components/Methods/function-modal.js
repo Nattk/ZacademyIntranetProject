d6794a52673d2a357af1d22c29c593ae74cc2b2d
souhail <mr.souid@live.fr>
2019-12-17 09:18:00 +0100

    clear some file

--- client/components/Methods/function-modal.js
+++
@@ -1,8 +0,0 @@
-export const handleClose = (updateState) => {
-  updateState({ showModal: false, title: '', content: '', titleValidation: '', contentValidation: '' })
-}
-export const handleModalAdd = (updateState) => {
-  updateState({
-    showModal: true, formulaire: true, formulaireTitleAdd: true, recap: false, formulaireUpdate: false, descriptionDelete: false, showDetails: false, title: '', content: '', avatar: '', titleValidation: '', contentValidation: ''
-  })
-}
