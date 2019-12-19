3af0c1fb39f023c339d59b00c488a3a900beea3d
zephimir <Zephimir>
2019-12-19 13:44:56 +0100

    carousel working

--- client/components/Carousel/carousel.js
+++ client/components/Carousel/carousel.js
@@ -1,27 +1,35 @@
 import React from 'react'
+import AllNotification from '../Notifications/notifications'
 
-const Carousel = ({ photo1, photo2, photo3, photo4 }) => {
+const Carousel = ({ photo1, photo2, photo3, photo4, alertType, show, notifMessage }) => {
   return (
     <article className="container fluid carousel-promo">
+      <AllNotification alertType={alertType} show={show} notifMessage={notifMessage}/>
       <section id="carouselExampleCaptions" className="carousel slide" data-interval="false" alt="carousel formation">
         <ol className="carousel-indicators">
         </ol>
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img src={photo1 || '/slide_1.png'}
+            <img src={photo1 || '/slide_1.jpg'}
               className="d-block w-100 carousel-connected " alt="carousel slide 1" />
             <div className="carousel-caption d-none d-md-block">
             </div>
           </div>
           <div className="carousel-item ">
-            <img src={photo2 || '/slide_2.png'}
+            <img src={photo2 || '/slide_2.jpg'}
               className="d-block w-100 carousel-connected " alt="carousel slide 2" />
             <div className="carousel-caption d-none d-md-block">
             </div>
           </div>
           <div className="carousel-item ">
-            <img src={photo3 || '/slide_3.png'}
+            <img src={photo3 || '/slide_3.jpg'}
               className="d-block w-100 carousel-connected " alt="carousel slide 3" />
+            <div className="carousel-caption d-none d-md-block">
+            </div>
+          </div>
+          <div className="carousel-item ">
+            <img src={photo4 || '/slide_3.jpg'}
+              className="d-block w-100 carousel-connected " alt="carousel slide 4" />
             <div className="carousel-caption d-none d-md-block">
             </div>
           </div>
