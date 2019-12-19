061e94fbe1d604497e0c56619ac40672a719a062
Nattan <kifoyinattan@gmail.com>
2019-12-19 20:37:16 +0100

    css gestion utilisateurs

--- client/pages/admin/gestion-utilisateur/gestion-utilisateur.js
+++ client/pages/admin/gestion-utilisateur/gestion-utilisateur.js
@@ -57,123 +57,48 @@ const UtilisateursGestion = () => {
   }
 
   return (
-    <Page title="gestion Utilisateur">
-      <article className="gestionProgramme card">
-        <header className="card-header">Liste des Utilisateurs</header>
-        <AllNotification alertType={errorStyle ? 'danger' : 'success'} show={notifShow} notifMessage={notifMessage} />
-        <section className="card-body">
-          {utilisateurs ? Array.from(utilisateurs).map((utilisateur, index) => (
-            <div key={index} className="d-flex flex- justify-content-around align-items-baseline">
-              <a href="#">
-                {utilisateur.firstName} {utilisateur.lastName}
-              </a>
-              <Button btnType="annuler" clicked={() => handleModal(utilisateur.id)}>
+    <Page title="gestion Utilisateur" contextePage="Gestion utilisateur">
+      <article id="liste-utilisateur" className="d-flex flex-column align-items-center">
+        <div className="card">
+          <header className="card-header text-center">Liste des Utilisateurs</header>
+          <AllNotification alertType={errorStyle ? 'danger' : 'success'} show={notifShow} notifMessage={notifMessage} />
+          <section className="card-body">
+            {utilisateurs ? Array.from(utilisateurs).map((utilisateur, index) => (
+              <div key={index} className="d-flex flex- justify-content-between align-items-baseline">
+                <a href="#">
+                  {utilisateur.firstName} {utilisateur.lastName}
+                </a>
+                <Button btnType="annuler" clicked={() => handleModal(utilisateur.id)}>
 								Supprimer
 	                </Button>
-              {modalShow ? (
-                <Alert
-                  show={modalShow}
-                  handleClose={handleClose}
-                  handleDelete={e => handleDelete(e)}
-                  headerTitle="Suppression utilisateur"
-                  modalDescription="Etes vous sûr de vouloir supprimer cet utilisateur ?"
-                  modalHeader={true}
-                  modalBody={true}
-                  modalFooter={true}
-                />
-              ) : null}
+                {modalShow ? (
+                  <Alert
+                    show={modalShow}
+                    handleClose={handleClose}
+                    handleDelete={e => handleDelete(e)}
+                    headerTitle="Suppression utilisateur"
+                    modalDescription="Etes vous sûr de vouloir supprimer cet utilisateur ?"
+                    modalHeader={true}
+                    modalBody={true}
+                    modalFooter={true}
+                  />
+                ) : null}
 
-            </div>
-          )
-          ) : null}
-        </section>
-        <footer className="d-flex flex-row align-items-end justify-content-center">
+              </div>
+            )
+            ) : null}
+          </section>
+          <footer className="d-flex flex-row align-items-end justify-content-center">
 
-          <a href="/admin/creation-utilisateur/creation-utilisateur" title="modification-utlisateur" className="link-button-creation" >
+            <a href="/admin/creation-utilisateur/creation-utilisateur" title="modification-utlisateur" className="link-button-creation" >
 						Ajouter un utilisateur
 	          </a>
 
-        </footer>
+          </footer>
+        </div>
       </article>
     </Page>
   )
 }
-
-// class UtilisateursGestion extends Component {
-// 	state = {
-// 	  utilisateurs: false,
-// 	  modalShow: false,
-// 	  userToDelete: '',
-// 	  show: false
-// 	}
-
-// 	componentDidMount () {
-// 	  userService.getAll().then(data => this.setState({ utilisateurs: data }))
-// 	  }
-
-// 	handleModal = (id) => {
-// 	  this.setState({ userToDelete: id })
-// 	  this.setState({ modalShow: true })
-// 	  event.preventDefault()
-// 	}
-
-// 	handleDelete = (e, id) => {
-// 	  e.preventDefault()
-// 	  userService.remove(this.state.userToDelete)
-// 	  userService.getAll().then(data => this.setState({ utilisateurs: data }))
-// 	  this.setState({ modalShow: false })
-
-// 	  event.preventDefault()
-// 	}
-
-// 	handleClose = (event) => {
-// 	  this.setState({ modalShow: false })
-
-// 	  event.preventDefault()
-// 	}
-
-// 	render () {
-// 	  return (
-// 	    <Page title="gestion Utilisateur">
-// 	      <article className="gestionProgramme card">
-// 	        <header className="card-header">Liste des Utilisateurs</header>
-// 	        <section className="card-body">
-// 	            {this.state.utilisateurs ? Array.from(this.state.utilisateurs).map((utilisateur, index) => (
-// 	              <div key={index} className="d-flex flex- justify-content-around align-items-baseline">
-// 	                <a href="#">
-// 	                  {utilisateur.firstName} {utilisateur.lastName}
-// 	                </a>
-// 	                <Button btnType="annuler" clicked={() => this.handleModal(utilisateur.id)}>
-// 										Supprimer
-// 	                </Button>
-// 	                {this.state.modalShow ? (
-// 	                  <Alert
-// 	                    show={this.state.modalShow}
-// 	                    handleClose={this.handleClose}
-// 	                    handleDelete={e => this.handleDelete(e, utilisateur.id)}
-// 	                    headerTitle="Suppression utilisateur"
-// 	                    modalDescription="Etes vous sûr de vouloir supprimer cet utilisateur ?"
-// 	                    modalHeader={true}
-// 	                    modalBody={true}
-// 	                    modalFooter={true}
-// 	                  />
-// 	                ) : null}
-
-// 	              </div>
-// 	            )
-// 	           ) : null}
-// 	        </section>
-// 	        <footer className="d-flex flex-row align-items-end justify-content-center">
-
-// 	          <a href="/admin/creation-utilisateur/creation-utilisateur" title="modification-utlisateur" className="link-button-creation" >
-// 							Ajouter un utilisateur
-// 	          </a>
-
-// 	        </footer>
-// 	      </article>
-// 	    </Page>
-// 	  )
-// 	}
-// }
 
 export default UtilisateursGestion
